b9b20d76d273f5df8c2fc284e22fa6c519b5b672
tanzeemc25 <tanzeem.c25@gmail.com>
2015-03-11 11:59:22 -0400

    Changed to more efficient helper methods

--- SudokuSolver.py
+++ SudokuSolver.py
@@ -19,79 +19,87 @@ def read_puzzle(filename):
     return puzzle_rows
 
 
-# Given a puzzle list containing lists of rows, returns a list containing list of columns
-def get_puzzle_columns(puzzle_rows):
+# Given a puzzle, returns whether all rows are valid, that is, true if all rows do not contain
+# empty spaces (represented by 0) and do not contain duplicates
+def check_rows(puzzle):
     
-    puzzle_columns = [[],[],[],[],[],[],[],[],[]]
-    
-    # From each row, iterate through the cell values to append them separatly to each of the column 
-    # lists
-    for row in puzzle_rows:
-        i = 0
-        while i < 9:
-            puzzle_columns[i].append(row[i])
-            i += 1
-        
-    return puzzle_columns
+    for row in puzzle:
 
-
-# Given a puzzle list containing lists of rows, returns a list containing lists of houses, i.e.
-# lists containing cell values that belong to same 3x3 sub-square
-def get_puzzle_houses(puzzle_rows):
-    
-    # Use p to make the list map look cleaner
-    p = puzzle_rows;
-
-    # Map the cell values in lists organised by the 3x3 house they belong to.
-    # Retrive cell value using row/column positon, and map acordingly
-    puzzle_houses = [   [ p[0][0], p[0][1], p[0][2], p[1][0], p[1][1], p[1][2], p[2][0], p[2][1], p[2][2] ],
-                        [ p[0][3], p[0][4], p[0][5], p[1][3], p[1][4], p[1][5], p[2][3], p[2][4], p[2][5] ],
-                        [ p[0][6], p[0][7], p[0][8], p[1][6], p[1][7], p[1][8], p[2][6], p[2][7], p[2][8] ],
-
-                        [ p[3][0], p[3][1], p[3][2], p[4][0], p[4][1], p[4][2], p[5][0], p[5][1], p[5][2] ],
-                        [ p[3][3], p[3][4], p[3][5], p[4][3], p[4][4], p[4][5], p[5][3], p[5][4], p[5][5] ],
-                        [ p[3][6], p[3][7], p[3][8], p[4][6], p[4][7], p[4][8], p[5][6], p[5][7], p[5][8] ],
-
-                        [ p[6][0], p[6][1], p[6][2], p[7][0], p[7][1], p[7][2], p[8][0], p[8][1], p[8][2] ],
-                        [ p[6][3], p[6][4], p[6][5], p[7][3], p[7][4], p[7][5], p[8][3], p[8][4], p[8][5] ],
-                        [ p[6][6], p[6][7], p[6][8], p[7][6], p[7][7], p[7][8], p[8][6], p[8][7], p[8][8] ]
-                    ]
-
-    return puzzle_houses    
-    
-
-# Given the puzzle's rows, columns, and houses, returns whether the puzzle is in goal state / solved.
-def is_goal_state(rows, columns, houses):
-
-    # If any row contains 0, or it's set (row with duplicates removed) doesn't have length of 9 - 
-    # meaning there were duplicates, return false
-    for row in rows:
+        # Get set of list. If set contains 0 (empty space), or the set does not have length 9, 
+        # meaning there were duplicates in the row, the row is invalid, so return false
         row_set = set(row)
         if 0 in row_set or len(row_set) != 9:
             return False
 
-    # If any column contains 0, or it's set (row with duplicates removed) doesn't have length of 9 - 
-    # meaning there were duplicates, return false
-    for column in columns:
+    # If code reaches here, all rows are valid, therefore return true
+    return True
+
+
+# Given a puzzle, returns whether all columns are valid, that is, true if all columns do not
+# contain empty spaces (represented by 0) and do not contain duplicates
+def check_columns(puzzle):
+
+    # Iterate to check each column
+    i = 0
+    while i < 9:
+
+        # Puzzle is stored as list of rows. So get columns by pulling values from all rows at the same
+        # index
+        column = [  puzzle[0][i], puzzle[1][i], puzzle[2][i], puzzle[3][i], puzzle[4][i], 
+                    puzzle[5][i], puzzle[6][i], puzzle[7][i], puzzle[8][i] ]
+
+        # Get set of list. If set contains 0 (empty space), or the set does not have length 9, 
+        # meaning there were duplicates in the column, the column is invalid, so return false
         col_set = set(column)
         if 0 in col_set or len(col_set) != 9:
             return False
 
-    # If any house contains 0, or it's set (row with duplicates removed) doesn't have length of 9 - 
-    # meaning there were duplicates, return false
-    for house in houses:
-        house_set = set(row)
+        i += 1
+
+    # If code reaches here, all columns are valid, therefore return true
+    return True
+
+
+# Given a puzzle, returns whether all houses (3x3 sub square) are valid, that is, true if all 
+# houses do not contain empty spaces (represented by 0) and do not contain duplicates
+def check_houses(puzzle):
+
+    # Puzzle is stored as list of rows. Get the coordinates of the center cells of each 3x3 house
+    centers = [ (1,1), (1,4), (1,7), (4,1), (4,4), (4,7), (7,1), (7,4), (7,7) ]
+
+    # Iterate to check each house
+    i = 0
+    while i < len(centers):
+        
+        r = centers[i][0] # row coordinate
+        c = centers[i][1] # column coordinate
+        
+        # If we have the coordinates for the center of the house, we can get the coordinates of all
+        # cell values surrounding it. Get house by using these coordinates.
+        house = [   puzzle[r-1][c-1], puzzle[r-1][c], puzzle[r-1][c+1], puzzle[r][c-1], puzzle[r][c], 
+                    puzzle[r][c+1], puzzle[r+1][c-1], puzzle[r+1][c], puzzle[r+1][c+1] ]
+
+        # Get set of list. If set contains 0 (empty space), or the set does not have length 9,
+        # meaning there were duplicates in the house, the house is invalid, so return false
+        house_set = set(house)
         if 0 in house_set or len(house_set) != 9:
             return False
 
+        i += 1
+
+    # If code reaches here, all houses are valid, therefore return true
     return True
+
+
+def is_goal_state(puzzle):
+
+    # Puzzle is in goal state, i.e. solved, if all rows, columns and houses are valid
+    return check_rows(puzzle) and check_columns(puzzle) and check_houses(puzzle)
 
 
 # Main method
 if __name__ == "__main__":
+
+    puzzle = read_puzzle("examplePuzzle.txt")
+    print is_goal_state(puzzle)
     
-    rows = read_puzzle("examplePuzzle.txt")
-    columns = get_puzzle_columns(rows)
-    houses = get_puzzle_houses(rows)
-    
-    print is_goal_state(rows, columns, houses)
